0735773033540e7bb5019d08023900b22b2cfd74
Madison Alexander <159088794+MadisonAlex14@users.noreply.github.com>
2026-02-24 21:10:30 -0600

    Update Home component with new logo and layout

--- my-react-app/src/Home.jsx
+++ my-react-app/src/Home.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
-import Logo from './assets/1.png'   // Your main Home page logo
 import './App.css'
+import logo from "./assets/creditmaxing.png";
+import oneImage from "./assets/1.png";
+
+
 
 export default function Home() {
   return (
-    <div className="page home-container">
-      <div className="site-name">CreditMaxxing</div>
-      <p classname="p">------------------------------------------------------------</p>
-      <div className="site-tagline">Maximize your rewards. Manage your budget.</div>
+    <div className="home-page">
 
-      <img src={Logo} alt="Team 4 Logo" className="home-logo" />
+      <div className="site-name">CreditMaxing</div>
+      <div className="dottedLine">
+        ---------------------------------------------------------
+      </div>
+
+      <div className="site-tagline">
+        Maximize your rewards. Manage your budget.
+      </div>
+
+      <img 
+        src={oneImage} 
+        alt="CreditMaxing illustration" 
+        className="home-logo"
+      />
+
+
 
       <h1 className="home-heading">Welcome to Team 4</h1>
-      <p className="home-subheading">Your dashboard overview</p>
     </div>
-  )
+
+  );
 }
