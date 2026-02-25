7f1ce97d17c1a68e9e34cb6935f2acd1d8372855
Madison Alexander <159088794+MadisonAlex14@users.noreply.github.com>
2026-02-24 21:07:29 -0600

    Add Rewards component to display credit card rewards

---
+++ my-react-app/src/Rewards.jsx
@@ -0,0 +1,8 @@
+export default function Rewards() {
+  return (
+    <div className="rewards-page">
+      <h1>Rewards</h1>
+      <p>Here you’ll see all your credit card rewards.</p>
+    </div>
+  );
+}
